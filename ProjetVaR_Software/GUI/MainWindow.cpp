71da856356a3f67687b58705d5dd4930968f67a6
Eric Chauty <eric.chauty@gmail.com>
2014-02-25 23:10:20 +0100

    test new branch + more import

--- ProjetVaR_Software/GUI/MainWindow.cpp
+++ ProjetVaR_Software/GUI/MainWindow.cpp
@@ -4,6 +4,7 @@
 #include <QFile>
 #include <QFileDialog>
 #include <QTableWidgetItem>
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -29,37 +30,37 @@ MainWindow::~MainWindow()
 
 void MainWindow::importCSV()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, ("Open File"), "C:/", ("csv File(*.csv)"));
-    QString data;
-    QFile importedCSV(fileName);
-    QStringList rowOfData;
-    QStringList rowData;
-    data.clear();
-    rowOfData.clear();
-    rowData.clear();
-    int z;
+		QString fileName = QFileDialog::getOpenFileName(this, ("Open File"), "C:/", ("csv File(*.csv)"));
+		QString data;
+		QFile importedCSV(fileName);
+		QStringList rowOfData;
+		QStringList rowData;
+		//QDateTime firstDate;
+		//QDateTime lastDate;
+		data.clear();
+		rowOfData.clear();
+		rowData.clear();
 
-       if (importedCSV.open(QFile::ReadOnly))
-       {
-           data = importedCSV.readAll();
-           rowOfData = data.split("\r\n");
-           importedCSV.close();
-       }
-       qDebug() << "Données importées";
+		if (importedCSV.open(QFile::ReadOnly))
+			{
+				data = importedCSV.readAll();
+				rowOfData = data.split("\r\n");
+				importedCSV.close();
+			}
+		qDebug() << "Données importées";
 
 //////CREATION DU FICHIER DES DONNEES IMPORTEES
-	   QFile file("Qt.txt");
-	   // On ouvre notre fichier en lecture seule et on vérifie l'ouverture
-	   if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+		//Faire des noms aléatoires et uniques
+		QString name = "Qt.txt";
+		QFile file(name);
+		// On ouvre notre fichier en lecture seule et on vérifie l'ouverture
+		if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
 		   return;
-	   QTextStream flux(&file);
-	   flux.setCodec("UTF-8");
+		QTextStream flux(&file);
+		flux.setCodec("UTF-8");
 
-
-
-
-       //TODO : Add rows automaticaly according to the file imported
-       // Try to import with a file with a size lower or equals to 9*9
+		//TODO : Add rows automaticaly according to the file imported
+		// Try to import with a file with a size lower or equals to 9*9
 
 		// x = 1 to avoid the first line with labels
 		// rowOfData.size()-1 to avoid a blank line a the end of the file
@@ -67,17 +68,6 @@ void MainWindow::importCSV()
 		{
            rowData = rowOfData.at(x).split(",");
 
-           //TODO : find the index of the column we want to import
-		/*
-           for (int y = 0; y < rowData.size(); y++)
-           {
-                /*
-               if ((x==0) && (rowData[y] == "Adj Close")){
-                   z = y;
-                   break;
-               }
-
-           */
 			   qDebug() << rowData[6];
                //  ui->tableWidget->insertRow(indice);
                QTableWidgetItem* item = new QTableWidgetItem();
@@ -87,7 +77,11 @@ void MainWindow::importCSV()
 			   // Écriture des différentes lignes dans le fichier, mais il devient imcompatible avec l'importation
 			   flux << rowData[0] << "," << rowData[6] << "\n";
 			   ui->tableWidget->setItem(x-1,0,item);
+		}
+		//obligé de faire des conversions pour passer de QString à QDateTime
+		//firstDate = rowOfData.at(1).split(",")[0];
+		//lastDate = rowOfData.at(rowOfData.size()-1).split(",")[0];
 
-	   }
+		//Asset a1 = Asset("name",name,firstDate,lastDate);
 }
 
