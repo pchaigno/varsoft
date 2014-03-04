75f032f00a767f5d1add8da286f312f0c43f4774
Ulysse <ulysses.goarant@gmail.com>
2014-03-04 15:00:57 +0100

    debut ecriture execute de varhisto

--- ProjetVaR_Software/ModelVaR/VaRHistorical.cpp
+++ ProjetVaR_Software/ModelVaR/VaRHistorical.cpp
@@ -5,7 +5,18 @@ VaRHistorical::VaRHistorical(const Portfolio& portfolio, double risk):
 
 }
 
-double VaRHistorical::execute(QDateTime timeHorizon) const {
-	// TODO
+double VaRHistorical::execute(int period) const {
+
+    QDateTime endingPeriodDate = portfolio.retrieveLastDate();
+    QDateTime startingPeriodDate = lastDate.addDays(-period+1);
+    QVector<double> values = portfolio.getAsQVectors(startingPeriodDate, endingPeriodDate);
+
+    if(startingPeriodDate < portfolio.retrieveFirstDate()) {
+        // Couille dans le piano
+    }
+
+    // Utilise qsort
+    values.
+
 	return 0;
 }
