add6c27dacd9134610ec391da0c173a73327108f
Benjamin Bouguet <benjamin.bouguet@insa-rennes.fr>
2014-03-20 13:53:47 +0100

    Add the calling of the external program to generate docx document.

--- ProjetVaR_Software/ModelVaR/DocxGenerator.cpp
+++ ProjetVaR_Software/ModelVaR/DocxGenerator.cpp
@@ -4,20 +4,23 @@
  * @brief Constructor
  * @param location The path to the DOCX file to generate.
  */
-DocxGenerator::DocxGenerator(QString location) {
-	// TODO
+DocxGenerator::DocxGenerator(QString location, QString templateFile, QJsonDocument doc) {
+    this->location=location;
+    this->templateFile=templateFile;
+    this->doc=doc;
 }
 
 /**
  * @brief Generates the DOCX file;
  */
 void DocxGenerator::generate() {
-	// TODO
-}
+    QProcess docx;
+    docx.start("java DocxGenerator", QStringList() << templateFile << location);
+    if (!docx.waitForStarted())
+     return false;
 
-/**
- * @brief Converts the DOCX file to a PDF.
- */
-void DocxGenerator::convertToPDF() {
-	// TODO
+    docx.write(doc.toJson(QJsonDocument::Compact));
+    docx.closeWriteChannel();
+
+    docx.waitForFinished();
 }
--- ProjetVaR_Software/ModelVaR/DocxGenerator.h
+++ ProjetVaR_Software/ModelVaR/DocxGenerator.h
@@ -1,11 +1,17 @@
 #pragma once
 
 #include <QString>
+#include <QProcess>
+#include <QJsonDocument>
 #include "ReportGenerator.h"
 
 class DocxGenerator: public ReportGenerator {
 public:
-	DocxGenerator(QString location);
+    DocxGenerator(QString location, QString templateFile,QJsonDocument doc);
 	virtual void generate();
 	void convertToPDF();
+
+private:
+    QString location, templateFile;
+    QJsonDocument doc;
 };
