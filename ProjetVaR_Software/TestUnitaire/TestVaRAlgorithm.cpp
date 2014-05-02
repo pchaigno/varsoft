38d7cbb27daad08857e0f42b02260f317e5811db
Ulysse <ulysses.goarant@gmail.com>
2014-05-02 17:55:05 +0200

    TestVaRAlgorithm removed

--- ProjetVaR_Software/TestUnitaire/TestVaRAlgorithm.cpp
+++
@@ -1,9 +0,0 @@
-#include "TestVaRAlgorithm.h"
-
-TestVaRAlgorithm::TestVaRAlgorithm(VaRAlgorithm * _algo){
-	this->algo = _algo;
-}
-
-void TestVaRAlgorithm::testVaRAlgorithmRisk() {
-	this->algo->execute(this->algo->getPortfolio().retrieveLastDate());
-}
