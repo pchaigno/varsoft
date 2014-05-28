2801ed1c2c9e43734d60c57505bcfbeb4dfe70af
Ulysse <ulysses.goarant@gmail.com>
2014-05-28 21:35:20 +0200

    Garch backtesting added

--- Sources/GUI/BacktestingDialog.cpp
+++ Sources/GUI/BacktestingDialog.cpp
@@ -5,6 +5,7 @@ BacktestingDialog::BacktestingDialog(QWidget *parent, Portfolio *portfolio) : QD
 {
 	ui->setupUi(this);
 	this->portfolio = portfolio;
+	initDates();
 }
 
 BacktestingDialog::~BacktestingDialog()
@@ -63,10 +64,61 @@ void BacktestingDialog::on_pushButton_run_backtesting_riskmetrics_clicked() {
 	msgBox.exec();
 }
 
+void BacktestingDialog::on_pushButton_run_backtesting_garch_clicked() {
+	int nbDaysLossGreaterThanVaR = 0;
+
+	try {
+		QPair<QDate, QDate> modelPeriod;
+		QDate modelStartDate = ui->startDatePeriodBacktestingGarchModel->date();
+		QDate modelEndDate = ui->endDatePeriodBacktestingGarchModel->date();
+		modelPeriod.first = modelStartDate;
+		modelPeriod.second = modelEndDate;
+
+		QPair<QDate, QDate> backtestPeriod;
+		QDate backtestStartDate = ui->startDatePeriodBacktestingGarch->date();
+		QDate backtestEndDate = ui->endDatePeriodBacktestingGarch->date();
+		backtestPeriod.first = backtestStartDate;
+		backtestPeriod.second = backtestEndDate;
+
+		GarchModel model = RInterface::computeGarchModel(*portfolio, modelPeriod);
+
+		VaRGarch algo(*portfolio, ui->riskBacktestingGarch->value(), ui->timeHorizonBacktestingGarch->value(), model, ui->bootstrapIterationsBacktestingGarch->value(), ui->initIterationsBacktestingGarch->value());
+		Backtesting backtesting(*portfolio, algo,backtestPeriod);
+		nbDaysLossGreaterThanVaR = backtesting.compute();
+
+	} catch(std::invalid_argument& ia) {
+		QMessageBox::warning(0, "Warning", QString(ia.what()));
+		return;
+	}
+
+	QMessageBox msgBox;
+	msgBox.setWindowTitle("Backtesting result");
+	msgBox.setText("Number of day(s) on which the loss was greater than the Value-at-Risk: "+QString::number(nbDaysLossGreaterThanVaR));
+	msgBox.exec();
+}
+
 void BacktestingDialog::on_pushButton_cancel_1_clicked() {
 	this->close();
 }
 
 void BacktestingDialog::on_pushButton_cancel_2_clicked() {
 	this->close();
+}
+
+void BacktestingDialog::on_pushButton_cancel_3_clicked() {
+	this->close();
+}
+
+void BacktestingDialog::initDates() {
+	QDate startDate = portfolio->retrieveStartDate();
+	ui->startDatePeriodBacktestingGarch->setDate(startDate);
+	ui->startDatePeriodBacktestingGarchModel->setDate(startDate);
+	ui->startDatePeriodBacktestingHistorical->setDate(startDate);
+	ui->startDatePeriodBacktestingRiskMetrics->setDate(startDate);
+
+	QDate endDate = portfolio->retrieveEndDate();
+	ui->endDatePeriodBacktestingGarch->setDate(endDate);
+	ui->endDatePeriodBacktestingGarchModel->setDate(endDate);
+	ui->endDatePeriodBacktestingHistorical->setDate(endDate);
+	ui->endDatePeriodBacktestingRiskMetrics->setDate(endDate);
 }
